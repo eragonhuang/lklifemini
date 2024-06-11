74a76390c3d09d83d47b32387891f7d0c50c127b
eragonhuang <eragonhuang@tencent.com>
2024-06-11 23:42:12 +0800

    up

--- pages/test/test.js
+++ pages/test/test.js
@@ -1,32 +1,74 @@
-//logs.js
-var util = require('../../utils/util.js')
 Page({
   data: {
-    slideWidth: '', //滑块宽
-    slideLeft: 0, //滑块位置
-    totalLength: '', //当前滚动列表总长
-    slideRatio: '' //滑块比例
+    listData: [{
+      id: '1',
+      name: '这是一个左滑删除功能',
+    }, {
+      id: '1',
+      name: '这是一个左滑删除功能',
+    }, {
+      id: '1',
+      name: '这是一个左滑删除功能',
+    }, {
+      id: '1',
+      name: '这是一个左滑删除功能',
+    }, {
+      id: '1',
+      name: '这是一个左滑删除功能',
+    }, ],
+
   },
-
-getRatio() {
-  var _totalLength =  1440; //总长度（单个块宽度*总数）
-  var _ratio = 100 / _totalLength * (750 / wx.getSystemInfoSync().windowWidth); //滚动列表长度与滑条长度比例
-  var _showLength = 750 / _totalLength * 100; //当前显示蓝色滑条的长度(保留两位小数)
-  this.setData({
-    slideWidth: _showLength,
-    totalLength: _totalLength,
-    slideRatio: _ratio
-  })
-},
-
-//slideLeft动态变化
-getleft(e) {
-  this.setData({
-   slideLeft: e.detail.scrollLeft * this.data.slideRatio
-  })
-},
-onLoad: function () {
-  this.getRatio()
-}
-
-})
+    /**
+   * 触摸开始
+   */
+    touchS: function (e) {
+      let index = e.currentTarget.dataset.index;
+      //遍历复位
+      this.data.listData.forEach(function (item, key) {
+        if (key != index) {
+          item.leftx = 0;
+        }
+      });
+      //复位后,赋值
+      this.setData({
+        listData: this.data.listData,
+      });
+      if (e.touches.length == 1) {
+        this.setData({
+          //设置触摸起始点水平方向位置
+          startX: e.touches[0].clientX
+        });
+      }
+    },
+    /**
+     * 触摸移动
+     */
+    touchM: function (e) {
+      let index = e.currentTarget.dataset.index;
+      var endX = e.touches[0].clientX;
+      var disX = this.data.startX - endX;
+      var pcontrolWidth = this.data.pcontrolWidth;
+      var left = 0;
+      //因为会和下滑有冲突，所以这里设置一定的距离
+      if (disX <= -20) {
+        left = 0;
+      } else if (disX > 30) {
+        left = 180;
+        // if (disX >= pcontrolWidth) {
+        //     //控制手指移动距离最大值为删除按钮的宽度
+        //     left = pcontrolWidth;
+        // }
+      }
+      this.data.listData[index].leftx = -left;
+      this.setData({
+        listData: this.data.listData,
+      });
+    },
+    /**
+     * 触摸结束
+     */
+    touchE: function (e) {
+  
+    },
+  
+});
