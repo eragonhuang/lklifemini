823af97fc1455df68754a2740f08df6b0411089a
eragonhuang <eragonhuang@tencent.com>
2024-06-14 00:44:14 +0800

    up

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -299,5 +299,59 @@ Page({
         checkedAllStatus: that.isCheckedAll()
       });
     });
-  }
+  },
+
+   /**
+   * 触摸开始
+   */
+   touchS: function (e) {
+    let index = e.currentTarget.dataset.index;
+    //遍历复位
+    this.data.cartGoods.forEach(function (item, key) {
+      if (key != index) {
+        item.leftx = 0;
+      }
+    });
+    //复位后,赋值
+    this.setData({
+      cartGoods: this.data.cartGoods,
+    });
+    if (e.touches.length == 1) {
+      this.setData({
+        //设置触摸起始点水平方向位置
+        startX: e.touches[0].clientX
+      });
+    }
+  },
+  /**
+   * 触摸移动
+   */
+  touchM: function (e) {
+    let index = e.currentTarget.dataset.index;
+    var endX = e.touches[0].clientX;
+    var disX = this.data.startX - endX;
+    var pcontrolWidth = this.data.pcontrolWidth;
+    var left = 0;
+    //因为会和下滑有冲突，所以这里设置一定的距离
+    if (disX <= -20) {
+      left = 0;
+    } else if (disX > 30) {
+      left = 180;
+      // if (disX >= pcontrolWidth) {
+      //     //控制手指移动距离最大值为删除按钮的宽度
+      //     left = pcontrolWidth;
+      // }
+    }
+    this.data.cartGoods[index].leftx = -left;
+    this.setData({
+      cartGoods: this.data.cartGoods,
+    });
+  },
+  /**
+   * 触摸结束
+   */
+  touchE: function (e) {
+
+  },
+
 })
